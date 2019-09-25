043f9c56eab9a13af330f4372bd482bb670c73a3
Matt Fysh <hi@mattfysh.com.au>
2019-09-25 11:12:14 +1000

    initial styling

---
+++ src/pages/Layout/MainLayout.svelte
@@ -0,0 +1,27 @@
+<div class="layout">
+  <header>The Chain Game</header>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  .layout {
+    --header-height: 70px;
+  }
+  header {
+    background-color: var(--blue);
+    color: var(--white);
+    text-transform: uppercase;
+    text-align: center;
+    line-height: var(--header-height);
+    font-weight: 800;
+    font-size: 18px;
+  }
+  main {
+    padding: 38px 28px;
+    min-height: calc(100vh - var(--header-height));
+    display: flex;
+    flex-direction: column;
+  }
+</style>
