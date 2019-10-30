657115c37b72b2ae0fb7ad16985a13eb1597bd4a
azichek <azichek@gmail.com>
2019-10-30 02:18:59 -0700

    Updated the welcome page
    Added words and some colors.....

--- src/pages/Welcome.svelte
+++ src/pages/Welcome.svelte
@@ -21,12 +21,20 @@
     }
   }
 </script>
-
 <PageWithAction>
   <div slot="content">
-    <h1>Welcome to Linked Up</h1>
-    <p>Grow your network by adding contacts</p>
-    <p>To add a contact, scan the QR code on another SFBC attendee's badge.</p>
+    <h2>Welcome to LinkedUp</h2>
+    <p>Grow your professional network, make friends, earn loot, and compete to take your share of $5,000! Meet with others, share your contact details, and build your reputation securely on the blockchain.</p>
+    <h3>Get Started</h3>
+    <p>To play, scan the QR code on another SFBW attendee's badge using your phone and share your contact details. For 
+      <font color ="3c48ea">
+        <b>iOS</b>
+      </font> devices: Due to less than ideal support for progressive web apps, it is recommended that you use 
+          
+      <font color ="3c48ea">
+        <b>Safari without a private tab</b>
+      </font> in order to have the smoothest LinkedUp experience.
+    </p>
     <p>Please verify your profile information and privacy settings</p>
   </div>
   <div slot="action">
@@ -34,4 +42,34 @@
       Verify your profile
     </Button>
   </div>
+  <style>
+  h1 {
+    color: var(--blue);
+    font-size: 36px;
+    line-height: 54px;
+    font-weight: bold;
+    position: absolute;
+    left: 0;
+    top: 65px;
+  }
+  h2 {
+    font-size: 24px;
+    font-weight: 500;
+    line-height: 36px;
+    margin: 1em 0 0.5em;
+    color: var(--blue);
+  }
+    h3 {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 30px;
+    margin: 1em 0 0.5em;
+    color: var(--blue);
+  }
+  p {
+    font-size: 14px;
+    line-height: 21px;
+    font-weight: normal;
+  }
+</style>
 </PageWithAction>
