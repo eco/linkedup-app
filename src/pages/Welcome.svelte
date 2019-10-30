9484445281f2f090da79dbabc88218319b38506a
Matt Fysh <hi@mattfysh.com.au>
2019-10-30 20:34:59 +1100

    patch text

--- src/pages/Welcome.svelte
+++ src/pages/Welcome.svelte
@@ -21,19 +21,27 @@
     }
   }
 </script>
+
 <PageWithAction>
   <div slot="content">
-    <h2>Welcome to LinkedUp</h2>
-    <p>Grow your professional network, make friends, earn loot, and compete to take your share of $5,000! Meet with others, share your contact details, and build your reputation securely on the blockchain.</p>
-    <h3>Get Started</h3>
-    <p>To play, scan the QR code on another SFBW attendee's badge using your phone and share your contact details. For 
-      <font color ="3c48ea">
-        <b>iOS</b>
-      </font> devices: Due to less than ideal support for progressive web apps, it is recommended that you use 
-          
-      <font color ="3c48ea">
-        <b>Safari without a private tab</b>
-      </font> in order to have the smoothest LinkedUp experience.
+    <h1>Welcome to LinkedUp</h1>
+    <p>
+      Grow your professional network, make friends, earn loot, and compete to
+      take your share of $5,000! Meet with others, share your contact details,
+      and build your reputation securely on the blockchain.
+    </p>
+    <h2>Get Started</h2>
+    <p>
+      To play, scan the QR code on another SFBW attendee's badge using your
+      phone and share your contact details.
+    </p>
+    <p>
+      For
+      <em>iOS</em>
+      devices: Due to less than ideal support for progressive web apps, it is
+      recommended that you use
+      <em>Safari without a private tab</em>
+      in order to have the smoothest LinkedUp experience.
     </p>
     <p>Please verify your profile information and privacy settings</p>
   </div>
@@ -42,34 +50,22 @@
       Verify your profile
     </Button>
   </div>
-  <style>
+</PageWithAction>
+
+<style>
   h1 {
     color: var(--blue);
-    font-size: 36px;
-    line-height: 54px;
-    font-weight: bold;
-    position: absolute;
-    left: 0;
-    top: 65px;
   }
   h2 {
-    font-size: 24px;
-    font-weight: 500;
-    line-height: 36px;
-    margin: 1em 0 0.5em;
-    color: var(--blue);
-  }
-    h3 {
     font-size: 18px;
     font-weight: 500;
     line-height: 30px;
     margin: 1em 0 0.5em;
     color: var(--blue);
   }
-  p {
-    font-size: 14px;
-    line-height: 21px;
+  em {
+    color: var(--blue);
+    font-style: normal;
     font-weight: normal;
   }
 </style>
-</PageWithAction>
