161f90d8c3a6b8bca6319a9bb4a9390a294cc93e
azichek <azichek@gmail.com>
2019-10-30 00:50:33 -0700

    Updated Welcome Page
    Added additional welcome text and formatting

--- src/pages/Welcome.svelte
+++ src/pages/Welcome.svelte
@@ -24,14 +24,37 @@
 
 <PageWithAction>
   <div slot="content">
-    <h1>Welcome to Linked Up</h1>
-    <p>Grow your network by adding contacts</p>
-    <p>To add a contact, scan the QR code on another SFBC attendee's badge.</p>
-    <p>Please verify your profile information and privacy settings</p>
-  </div>
-  <div slot="action">
-    <Button fullWidth on:click={verifyProfile} {loading}>
+    <h1>
+      <font color ="3c48ea">Welcome to LinkedUp</font>
+    </h1>
+    <p>Grow your professional network, make friends, earn loot, and compete to take your share of $5,000!</p>
+    <p>Meet with others, share your contact details, and build your reputation securely on the blockchain.</p>
+    <h3>
+      <font color ="3c48ea">Why blockchain?</font>
+    </h3>
+    <p>You get to choose 
+      <b>WHO</b> can see your data and how much of it. You and your contact's details stay hidden while your reputation and notoriety grow.
+    </p>
+    <p>Meet someone that you want to hang with after the conference? Share your telegram and mobile number.</p>
+    <p>Want to follow up with that sales director but on your terms? Share just your work email.
+      <p>
+        <h3>
+          <font color ="3c48ea">Get Started</font>
+        </h3>
+        <p>To play, scan the QR code on another SFBW attendee's badge using your phone and share your contact details</p>
+        <p>For 
+          <font color ="3c48ea">
+            <b>iOS</b>
+          </font> devices: Due to less than ideal support for progressive web apps, it is recommended that you use 
+          <font color ="3c48ea">
+            <b>Safari without a private tab</b>
+          </font> in order to have the smoothest LinkedUp experience.
+        </p>
+        <p>Please verify your profile information and privacy settings</p>
+      </div>
+      <div slot="action">
+        <Button fullWidth on:click={verifyProfile} {loading}>
       Verify your profile
     </Button>
-  </div>
-</PageWithAction>
+      </div>
+    </PageWithAction>
